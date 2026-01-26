93cdda47f7a90faa851494159c3cfb59d572eeb7
Rico <r@rico.wtf>
2026-01-26 08:49:36 +0100

    initial template

---
+++ lib/phoenix_datastar.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixDatastar do
+  @moduledoc """
+  Documentation for `PhoenixDatastar`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixDatastar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_datastar_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixDatastarTest do
+  use ExUnit.Case
+  doctest PhoenixDatastar
+
+  test "greets the world" do
+    assert PhoenixDatastar.hello() == :world
+  end
+end
