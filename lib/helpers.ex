809c44c91d6aebe845ff5b255b00955ac2104ed3
Rico <r@rico.wtf>
2026-02-12 09:46:35 +0100

    stringify conn.assigns keys for consistency

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -6,6 +6,10 @@ defmodule PhoenixDatastar.Helpers do
   @doc """
   Extracts the session map from a Plug.Conn.
 
+  Merges the Plug session (from the session store) with all `conn.assigns`
+  set by plugs in the pipeline. Assign keys are stringified so they can be
+  pattern-matched in `mount/3` the same way as regular session keys.
+
   Returns the session data if available, otherwise an empty map.
   """
   @spec get_session_map(Plug.Conn.t()) :: map()
@@ -17,15 +21,18 @@ defmodule PhoenixDatastar.Helpers do
         %{}
       end
 
-    # Include current_user and flash from conn assigns if available
-    session =
-      if conn.assigns[:current_user] do
-        Map.put(session, "current_user", conn.assigns[:current_user])
-      else
-        session
-      end
+    # Forward all conn.assigns into the session with string keys.
+    # This ensures any data set by plugs (current_user, current_scope, etc.)
+    # is available in the view's mount/3 callback.
+    conn_assigns =
+      conn.assigns
+      |> Map.drop([:layout])
+      |> Map.new(fn {k, v} -> {to_string(k), v} end)
 
-    Map.put(session, "flash", conn.assigns[:flash] || %{})
+    # Ensure flash always has a default value
+    conn_assigns = Map.put_new(conn_assigns, "flash", %{})
+
+    Map.merge(session, conn_assigns)
   end
 
   @doc """
